ca2d05591520e6bba1acacd37764649f3e5b84d2
Bleak-Dallas <dbleak42@gmail.com>
2016-02-09 22:03:24 -0700

    Added all functions related to MapControl Class

--- Adrift/src/byui/cit260/adrift/control/MapControl.java
+++ Adrift/src/byui/cit260/adrift/control/MapControl.java
@@ -5,10 +5,30 @@
  */
 package byui.cit260.adrift.control;
 
-/**
- *
- * 
- */
+
 public class MapControl {
     
+    public void moveToLocation(String actorAllName, double currentLocation, int mapLocation, int buggyLocatioon) {
+
+    }
+    
+    public void validLocation(String actorAllName, double currentLocation, int mapLocation, int buggyLocatioon) {
+
+    }
+    
+    public void visitResouceLocation(String actorAllName, double currentLocation, int mapLocation, int buggyLocatioon) {
+
+    }
+    
+    public void visitRegularLocation(String actorAllName, double currentLocation, int mapLocation, int buggyLocatioon) {
+
+    }
+    
+    public void visitShipLocation(String actorAllName, double currentLocation, int mapLocation, int buggyLocatioon) {
+
+    }
+    
+    public void visitFacilityLocation(String actorAllName, double currentLocation, int mapLocation, int buggyLocatioon) {
+
+    }
 }
