6582988057dd5f1879e4c239f0f5d38307d3fc0c
Bleak-Dallas <dbleak42@gmail.com>
2016-03-24 20:47:53 -0600

    Added finally statement to toolInventoryReport method.

--- Adrift/src/byui/cit260/adrift/view/PrintReportView.java
+++ Adrift/src/byui/cit260/adrift/view/PrintReportView.java
@@ -9,6 +9,7 @@ import adrift.Adrift;
 import byui.cit260.adrift.control.ToolsControl;
 import byui.cit260.adrift.model.Game;
 import byui.cit260.adrift.model.Tools;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
@@ -22,7 +23,7 @@ class PrintReportView extends View{
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_RESET = "\u001B[0m";
     Game game = Adrift.getCurrentGame();
-     Tools[] toolInventory = ToolsControl.getSortedToolList();
+    Tools[] toolInventory = ToolsControl.getSortedToolList();
     private static PrintWriter reportFile = null;
     
     public PrintReportView(){
@@ -68,47 +69,42 @@ class PrintReportView extends View{
     }
 
     private void toolInventoryReport() {
-        this.console.println("\nEnter the filepath where the report will be saved");
+        this.console.println(ANSI_BLUE + "\nEnter the filepath and file name where the report will be saved" + ANSI_RESET);
         
         String filePath = this.getInput();
         try(PrintWriter reportFile = new PrintWriter(filePath)) {
-//            if(filePath = true) {
-//                this.console.println("\nFile " + filePath + " already exists");
-//            }
+
         // print title and column headings
         reportFile.println("\n\n                   Tool Inventory Report                    ");
-        reportFile.printf("%n%-20s%10s%10s%15s", "Description", "In Stock", "Resource", "Amount Required");
+        reportFile.printf("%n%-15s%14s%18s%20s", "Description", "In Stock", "Resource", "Amount Required");
+        reportFile.printf("%n%-15s%14s%18s%20s", "-----------", "--------", "--------", "---------------");
         
         //print toool items
         for(Tools tool : toolInventory) {
-            reportFile.printf("%n%-20s%7d%10s%7d"   , tool.getDescription()
-                                             , tool.getQuantityInStock()
-                                             , tool.getRequiredResource()
-                                             , tool.getRequiredAmount());
+            reportFile.printf("%n%-15s%10d%23s%13d" , tool.getDescription()
+                                                    , tool.getQuantityInStock()
+                                                    , tool.getRequiredResource()
+                                                    , tool.getRequiredAmount());
         }
         if(reportFile != null) {
             this.console.println(ANSI_GREEN + "\n\nYour report has been succesfully run and saved" + ANSI_RESET);
         }
 
         }
-        catch(Throwable te) {
-            ErrorView.display("GameMenuView", te.getMessage());
+        catch(IOException ex) {
+            ErrorView.display("GameMenuView", ex.getMessage());
         }
         
-        if(reportFile != null) {
-            reportFile.close();
-        }
+        finally {
+                try {
+                    if(reportFile != null)
+                        reportFile.close();
 
-//        finally {
-//            try {
-//                if(reportFile != null)
-//                    reportFile.close();
-//
-//             } catch (IOException ex) {
-//                 ErrorView.display("GameMenuView", ex.getMessage());
-//             }
-//        
-//    }
+                 } catch (Exception ex) {
+                    ErrorView.display("GameMenuView Error closing file", ex.getMessage());
+                }  
+        
+        }
     }
 
     private void resourcesReport() {
