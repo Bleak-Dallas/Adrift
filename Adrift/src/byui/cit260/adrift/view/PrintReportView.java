b4e08dad55a33461415addb4fad44988cb13c18b
Johnmoney66 <bro13056@byui.edu>
2016-03-28 19:34:35 -0500

    ResourcesReport - Finalized

--- Adrift/src/byui/cit260/adrift/view/PrintReportView.java
+++ Adrift/src/byui/cit260/adrift/view/PrintReportView.java
@@ -10,6 +10,8 @@ import byui.cit260.adrift.control.GameControl;
 import byui.cit260.adrift.control.ToolsControl;
 import byui.cit260.adrift.model.Game;
 import byui.cit260.adrift.model.InventoryItem;
+import byui.cit260.adrift.model.Location;
+import byui.cit260.adrift.model.Map;
 import byui.cit260.adrift.model.Tools;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -25,8 +27,10 @@ class PrintReportView extends View{
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_RESET = "\u001B[0m";
     Game game = Adrift.getCurrentGame();
+    Map map= game.getMap();
     Tools[] toolInventory = ToolsControl.getSortedToolList();
     InventoryItem[] inventory = GameControl.getSortedInventoryList();
+    Location[][] locations = map.getLocations();
     private static PrintWriter reportFile = null;
     
     public PrintReportView(){
@@ -151,20 +155,23 @@ class PrintReportView extends View{
         try(PrintWriter reportFile = new PrintWriter(filePath)) {
 
         // print title and column headings
-        reportFile.println("\n\n                   Resources Report                    ");
-        reportFile.printf("%n%-15s%14s%18s%20s", "Description", "In Stock", "Resource", "Amount Required");
-        reportFile.printf("%n%-15s%14s%18s%20s", "-----------", "--------", "--------", "---------------");
+        reportFile.println("\n\n               Resources Report                 ");
+        reportFile.printf("%n%-15s%11s%18s", "Sector", "Description", "Available");
+        reportFile.printf("%n%-15s%11s%18s", "------", "-----------", "---------");
         
         //print Resources
-        for(Tools tool : toolInventory) {
-            reportFile.printf("%n%-15s%10d%23s%13d" , tool.getDescription()
-                                                    , tool.getQuantityInStock()
-                                                    , tool.getRequiredResource()
-                                                    , tool.getRequiredAmount());
+        for(int row = 0; row < map.getLocations().length; row++) {
+            // Add header
+            for (int column = 0; column < map.getLocations()[row].length; column++)  {
+            reportFile.printf("%n%-15s%-15s%10d" , row + "," + column  
+                                                    , locations[row][column].getScene().getResourceDescription().trim()
+                                                    , locations[row][column].getScene().getResourceAmount());
+        }
         }
         if(reportFile != null) {
             this.console.println(ANSI_GREEN + "\n\nYour report has been succesfully run and saved" + ANSI_RESET);
         }
+       
 
         }
         catch(IOException ex) {
